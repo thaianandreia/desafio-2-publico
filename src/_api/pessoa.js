ff19bef4dd58104e23d3525ff9e964d451346808
otaaviostein <otavio.stein@gmail.com>
2022-05-15 21:56:27 -0300

    resolucao

--- src/_api/pessoa.js
+++ src/_api/pessoa.js
@@ -8,22 +8,34 @@ export default {
   GetById: function (id) {
     return new Promise((resolve, reject) => {
       var pessoas = getListaPessoas()
-      resolve(pessoas.find(p => p.id === id))
+      resolve(pessoas.body.find(p => p.id === id))
     })
   },
   Post: function (data) {
     return new Promise((resolve, reject) => {
       var pessoas = getListaPessoas()
-      // Insira aqui o seu código: adicionar item na lista
+      const maxId = pessoas.body.reduce(
+        (max, item) => (item.id > max ? item.id : max),
+        pessoas.body[0].id
+      );
+      
+      const newData = {
+        ...data,
+        id: maxId + 1
+      }
 
+      pessoas.body.push(newData);
       setListaPessoas(pessoas)
-      resolve(data)
+      resolve(pessoas)
     })
   },
   Put: function (id, data) {
     return new Promise((resolve, reject) => {
       var pessoas = getListaPessoas()
-      // Insira aqui o seu código: encontre o item na lista e o altere
+      const pessoaIndex = pessoas.body.findIndex(pessoa => pessoa.id === id);
+      
+      pessoas.body[pessoaIndex] = data;
+
       setListaPessoas(pessoas)
       resolve(data)
     })
@@ -31,7 +43,13 @@ export default {
   Delete: function (id) {
     return new Promise((resolve, reject) => {
       var pessoas = getListaPessoas()
-      // Insira aqui o seu código: exclua o item da lista
+      const index = pessoas.body.findIndex(pessoa => pessoa.id === id);
+
+      if(index >= 0) {
+        pessoas.body.splice(index, 1);
+      } else {
+        return
+      }
 
       setListaPessoas(pessoas)
       resolve(id)
@@ -60,16 +78,59 @@ function setListaPessoas (pessoas) {
   }
 }
 function carregaPessoas () {
-  return [
-    { id: 1, nome: 'Maria Lúcia Santana', idade: 20, email: 'maria.lucia@gmail.com', telefone: '(47)34850-8287', cidade: 'Joinville' },
-    { id: 2, nome: 'Paulo Agusto da Silva', idade: 30, email: 'paulo.augusto@yahoo.com', telefone: '(11) 26492-8474', cidade: 'Jundiaí' },
-    { id: 3, nome: 'Maria Joaquina da Costa', idade: 25, email: 'maria.joaquina@gmail.com', telefone: '(83) 42069-5692', cidade: 'Sorocaba' },
-    { id: 4, nome: 'Júlia Andrade Aquino', idade: 32, email: 'julia.andrade@hotmail.com', telefone: '(47)34850-8287', cidade: 'Itajaí' },
-    { id: 5, nome: 'Matheus Cardoso Faria', idade: 45, email: 'matheus.cardoso@yahoo.com', telefone: '(31) 26441-2187', cidade: 'Joinville' },
-    { id: 6, nome: 'Otávio Andrade', idade: 32, email: 'otavio.andrade@gmail.com', telefone: '(51) 78144-1699', cidade: 'Chapecó' },
-    { id: 7, nome: 'Pedro José Vidal', idade: 43, email: 'pedro.jose@hotmail.com', telefone: '(95) 23511-8084', cidade: 'Joinville' },
-    { id: 8, nome: 'Maria Luiza Pereira', idade: 12, email: 'maria.luiza@hotmail.com', telefone: '(84) 69919-3869', cidade: 'Maringá' },
-    { id: 9, nome: 'Vinicius dos Santos', idade: 32, email: 'vinicius.santos@hotmail.com', telefone: '(79) 36607-8646', cidade: 'Joinville' },
-    { id: 10, nome: 'Flávia Regina Pedrosa', idade: 34, email: 'flavia.regina@yahoo.com', telefone: '(83) 32878-5703', cidade: 'Rio de Janeiro' }
-  ]
+  return {
+    headers: [
+      {
+        text: 'Id',
+        align: 'start',
+        sortable: false,
+        value: 'id',
+      },
+      {
+        text: 'Nome',
+        align: 'start',
+        sortable: false,
+        value: 'name',
+      },
+      {
+        text: 'Idade',
+        align: 'start',
+        sortable: false,
+        value: 'age',
+      },
+      {
+        text: 'E-mail',
+        align: 'start',
+        sortable: false,
+        value: 'email',
+      },
+      {
+        text: 'Telefone',
+        align: 'start',
+        sortable: false,
+        value: 'phone',
+      },
+      {
+        text: 'Cidade',
+        align: 'start',
+        sortable: false,
+        value: 'city',
+      },
+      { text: "Editar", value: "edit", sortable: false, filterable: false },
+      { text: "Excluir", value: "delete", sortable: false, filterable: false },
+    ],
+
+    body: [
+      { id: 1, name: 'Maria Lúcia Santana', age: 20, email: 'maria.lucia@gmail.com', phone: '(47)34850-8287', city: 'Joinville' },
+      { id: 2, name: 'Paulo Agusto da Silva', age: 30, email: 'paulo.augusto@yahoo.com', phone: '(11) 26492-8474', city: 'Jundiaí' },
+      { id: 3, name: 'Maria Joaquina da Costa', age: 25, email: 'maria.joaquina@gmail.com', phone: '(83) 42069-5692', city: 'Sorocaba' },
+      { id: 4, name: 'Júlia Andrade Aquino', age: 32, email: 'julia.andrade@hotmail.com', phone: '(47)34850-8287', city: 'Itajaí' },
+      { id: 5, name: 'Matheus Cardoso Faria', age: 45, email: 'matheus.cardoso@yahoo.com', phone: '(31) 26441-2187', city: 'Joinville' },
+      { id: 6, name: 'Otávio Andrade', age: 32, email: 'otavio.andrade@gmail.com', phone: '(51) 78144-1699', city: 'Chapecó' },
+      { id: 7, name: 'Pedro José Vidal', age: 43, email: 'pedro.jose@hotmail.com', phone: '(95) 23511-8084', city: 'Joinville' },
+      { id: 8, name: 'Maria Luiza Pereira', age: 12, email: 'maria.luiza@hotmail.com', phone: '(84) 69919-3869', city: 'Maringá' },
+      { id: 9, name: 'Vinicius dos Santos', age: 32, email: 'vinicius.santos@hotmail.com', phone: '(79) 36607-8646', city: 'Joinville' },
+      { id: 10, name: 'Flávia Regina Pedrosa', age: 34, email: 'flavia.regina@yahoo.com', phone: '(83) 32878-5703', city: 'Rio de Janeiro' }
+    ]
+  }
 }
--- src/views/PessoaView.vue
+++ src/views/PessoaView.vue
@@ -1,22 +1,161 @@
 <template>
   <div>
-    <p v-for="(pessoa, i) in pessoas" :key="i">
-      {{pessoa.id}} - {{pessoa.nome}}
-    </p>
+     <v-btn
+      color="primary"
+      elevation="2"
+      @click.stop="abrirModalPessoa">
+      NOVA PESSOA
+    </v-btn>
+
+    <v-data-table
+      :headers="pessoas.headers"
+      :items="pessoas.body"
+      :items-per-page="5"
+      class="elevation-1"
+    >
+      <template v-slot:[`item.edit`]="{ item }">
+        <v-btn depressed color="primary" @click="abrirModalPessoa(item)">
+          Editar
+        </v-btn>
+      </template>
+      <template v-slot:[`item.delete`]="{ item }">
+        <v-btn depressed color="error" @click="excluirUsuario(item)">
+          Excluir
+        </v-btn>
+      </template>
+    
+    </v-data-table>
+
+    <pessoa-modal 
+      v-model="mostrarModal"
+      :usuarioSelecionado="usuarioSelecionado"
+      :regras="rules"
+      :edicao="edicao"
+      @cadastrar="cadastrar"
+      @editar="editar"
+    >
+    </pessoa-modal>
+    <pessoa-modal-exclusao
+      v-model="modalExclusao"
+      :usuarioId="usuarioSelecionado.id"
+      @confirmaExclusao="confirmaExclusao"
+    ></pessoa-modal-exclusao>
+
+
+    <v-snackbar
+      v-model="snackbar"
+      :vertical="vertical"
+    >
+      {{ mensagem }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="indigo"
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
+        >
+          Fechar
+        </v-btn>
+      </template>
+    </v-snackbar>
+
   </div>
 </template>
 <script>
+import PessoaModal from './PessoaModal.vue'
+import PessoaModalExclusao from './PessoaModalExclusao.vue'
 export default {
-  data: function () {
-    return {
-      pessoas: []
-    }
-  },
+  components: { PessoaModal, PessoaModalExclusao },
+  data: () => ({
+    rules: {
+      regraNome: [
+        v => !!v || 'Este campo é obrigatório',
+      ],
+      regraEmail: [
+        v => !!v || 'Este campo é obrigatório',
+        v => /.+@.+\..+/.test(v) || 'Formato de email inválido',
+      ],
+      regraIdade: [
+        v => !!v || 'Este campo é obrigatório',
+      ],
+      regraCidade: [
+        v => !!v || 'Este campo é obrigatório',
+      ],
+      regraTelefone: [
+        v => !!v || 'Este campo é obrigatório',
+        v => /(?:\()[0-9]{2}(?:\))\s?[0-9]{4,5}(?:-)[0-9]{4}$/.test(v) || 'Telefone inválido',
+      ],
+    },
+    snackbar: false,
+    mensagem: '',
+    vertical: true,
+    edicao: false,
+    usuarioSelecionado: {},
+    dialog: false,
+    modalExclusao: false,
+    mostrarModal: false,
+    pessoas: [],
+    name: '',
+    email: '',
+    city: '',
+    phone: '',
+    age: 0,
+    id: 0
+  }),
   mounted () {
-    const th = this
-    th.$api.Pessoa.GetAll().then(function (response) {
-      th.pessoas = response
-    })
+    this.fetch()
+  },
+  methods: {
+    fetch() {
+       const th = this
+        th.$api.Pessoa.GetAll().then((response) => {
+          th.pessoas = response
+        })
+    },
+    editar() {
+      this.$api.Pessoa.Put(this.usuarioSelecionado.id, this.usuarioSelecionado).then((response) => {
+        this.mostrarModal = false
+        this.fetch();
+        this.snackbar = true
+        this.mensagem = 'Pessoa editada com sucesso'
+      })
+    },
+    cadastrar() {
+      this.$api.Pessoa.Post(this.usuarioSelecionado).then((response) => {
+        this.mostrarModal = false
+        this.fetch();
+        this.snackbar = true
+        this.mensagem = 'Pessoa cadastrada com sucesso'
+      })
+    },
+    excluirUsuario(user) {
+      this.modalExclusao = true;
+      this.usuarioSelecionado = user;
+    },
+    confirmaExclusao() {
+      const th = this;
+      th.$api.Pessoa.Delete(th.usuarioSelecionado.id).then((response) => {
+        this.modalExclusao = false
+        this.fetch();
+        this.snackbar = true
+        this.mensagem = 'Pessoa excluída com sucesso'
+      })
+    },
+    abrirModalPessoa(item) {
+      if (item.id) {
+        this.$api.Pessoa.GetById(item.id).then((response) => {
+          this.usuarioSelecionado = response;
+          this.edicao = true;
+        })
+        
+      } else {
+        this.usuarioSelecionado = {};
+        this.edicao = false;
+      }
+
+      this.mostrarModal = true;
+    }
   }
 }
 </script>
